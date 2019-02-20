b7a43785d83c71d485172797ac806a8072886fd0
Jason Stratman <jason@stratmanmedia.com>
2019-02-20 17:44:55 -0600

    Moved connection string to user secrets

--- SecretSantaApp.Api/Startup.cs
+++ SecretSantaApp.Api/Startup.cs
@@ -20,8 +20,9 @@ namespace SecretSantaApp.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration["secretSantaConnection"];
             services.AddDbContext<SecretSantaContext>(
-                options => options.UseSqlServer("Server = tcp:secretsantacpt200.database.windows.net, 1433; Initial Catalog = SecretSanta; Persist Security Info = False; User ID =secretsantaadmin; Password =bgofFN1RQiXh061L; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;"));
+                options => options.UseSqlServer(connectionString));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
