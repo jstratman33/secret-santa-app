41a33a869ebab1b35174ef2b4956ba427a95f09d
Jason Stratman <jstratman@keefegroup.com>
2019-02-27 17:33:57 -0600

    added List respository

--- SecretSantaApp.EfCore/Repositories/ListRepository.cs
+++ SecretSantaApp.EfCore/Repositories/ListRepository.cs
@@ -1,4 +1,5 @@
-﻿using SecretSantaApp.EfCore.Enitities;
+﻿using System.Linq;
+using SecretSantaApp.EfCore.Enitities;
 
 namespace SecretSantaApp.EfCore.Repositories
 {
@@ -6,6 +7,21 @@ namespace SecretSantaApp.EfCore.Repositories
     {
         public ListRepository(SecretSantaContext context) : base(context)
         {
+        }
+
+        public List Get(long id)
+        {
+            return Context.Lists.First(l => l.Id == id);
+        }
+
+        public List[] GetAllByUserId(long id)
+        {
+            return Context.Lists.Where(l => l.OwnerId == id).ToArray();
+        }
+
+        public List[] GetAllByGroupId(long id)
+        {
+            return Context.Lists.Where(l => l.GroupId == id).ToArray();
         }
     }
 }
