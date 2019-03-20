72bd7d2dbe1d682c0eb769f31847f4dfdc6c8f1b
Jason Stratman <jason@stratmanmedia.com>
2019-03-20 17:45:49 -0500

    Recreated Angular project

--- secret-santa-app-ng/e2e/src/app.e2e-spec.ts
+++ secret-santa-app-ng/e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('secret-santa-app-ng App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('secret-santa-app-ng App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to secret-santa-app-ng!');
   });
 });
