c8ae181e6402599c11eea86f802b3c794669c69b
Jason Stratman <jason@stratmanmedia.com>
2019-04-28 14:13:03 -0500

    added back end invite service

---
+++ SecretSantaApp.EfCore/Migrations/20190428172647_changed-hash-column-to-string.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecretSantaApp.EfCore.Migrations
+{
+    public partial class changedhashcolumntostring : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Hash",
+                table: "Invites",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Hash",
+                table: "Invites",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
