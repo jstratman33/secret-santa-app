e2c622c6a60012672967e0a7ef569225bd758869
Joanne-jjb <jjbuenaventura@gmail.com>
2019-05-06 17:57:30 -0500

    Adds user logic to getGroups

--- secret-santa-app-ng/src/app/dashboard/dashboard.component.ts
+++ secret-santa-app-ng/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user';
+import { GroupService } from '../services/group.service';
+import { Group } from '../models/group';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User = null;
+  groups: Group[];
+
+  constructor(private userService: UserService,
+    private groupService: GroupService) { }
 
   ngOnInit() {
+    this.userService.currentUser.subscribe((user: User) => {
+      this.currentUser = user;
+    });
+
+    this.groupService.getAllByUserId(this.currentUser.id).subscribe(res => {
+      console.log(JSON.stringify(res));
+      this.groups = res;
+    });
   }
 
 }
