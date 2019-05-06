00fceaa0f929b757516a3da7d7d62606b54d4c91
cmward96@gmail.com <cmward96@gmail.com>
2019-05-06 18:39:55 -0500

    update

--- secret-santa-app-ng/src/app/create-list/create-list.component.ts
+++ secret-santa-app-ng/src/app/create-list/create-list.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { GroupService } from '../services/group.service';
 import { Group } from '../models/group';
 import { List } from '../models/list';
+import { ListService } from 'c:/Users/sward_000/Documents/SecretSantaWorkspace/secret-santa-app-ng/src/app/services/list.service'
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-create-list',
@@ -11,24 +13,52 @@ import { List } from '../models/list';
 })
 export class CreateListComponent implements OnInit {
   private WishList=[];
-  constructor(private GroupService: GroupService) {
+  currentUser: User=null;
+  Groups: Group[]=[];
+  constructor(private listService: ListService,
+    private userService: UserService,
+    private groupService: GroupService) {
     this.AddList();
    }
 
   ngOnInit() {
+    this.userService.currentUser.subscribe((user: User)=>{
+    this.currentUser=user;
+    this.groupService.getAllByUserId(user.id).subscribe((groups: Group[])=>{
+      this.Groups=groups;
+    })
+    });
   }
   AddList(): void{
-    // this.WishList.push({
-    //   id: Date.now(),
-    //   item: ""
-    // });
+     this.WishList.push({
+       id: Date.now(),
+       item: ""
+     });
   }
-  SubmitForm(): void{
-    // this.WishList.forEach(x=>{
-    //   const Items: List={
-    //     id: 0,
-    //     item: ""
-    //   };
-    // });
+  SubmitList(): void{
+    this.WishList.forEach(x=>{
+      const List: List={
+          id: 0,
+          ownerId: this.currentUser.id,
+          santaId: 0,
+          groupId: this.groupId,
+          name: 'User\'s Wish List',
+          isPrimary: true,
+          items: [
+            {
+              id: 0,
+              listId: 0,
+              description: x.item,
+              isPurchased: false
+            },
+            {
+              id: 0,
+              listId: 0,
+              description: x.item,
+              isPurchased: false
+            }
+          ]
+        }  
+    });
   }
 }
